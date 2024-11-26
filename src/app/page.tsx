d9b55eacb9310254757bbdbdb4f42e5c58f40f19
faralha <faralhaonly@gmail.com>
2024-11-26 10:18:15 +0700

    fix: container landing page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,11 @@ export default async function Home() {
   return (
     <div>
       <Header />
-      <Carousel />
-      <ProductsPage products={products} />
+      <section className='p-4 2xl:mx-80 xl:mx-52 lg:mx-60 md:mx-32 sm:mx-12'>
+        <Carousel/>
+        <h1 className="text-2xl font-semibold tracking-tight pt-10">Trending Products</h1>
+        <ProductsPage products={products}/>
+      </section>
     </div>
   );
 }
